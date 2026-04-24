9c90d3855cb07e163ded631de009632fcb788931
Chromiumore <parts.dima06@gmail.com>
2026-04-24 18:53:09 +0300

    open comments check

--- Preprocessor/src/main/java/me/chromiumore/mtnc/Preprocessor.java
+++ Preprocessor/src/main/java/me/chromiumore/mtnc/Preprocessor.java
@@ -3,9 +3,11 @@ package me.chromiumore.mtnc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Preprocessor {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         if (args.length < 1) {
             throw new RuntimeException("Usage: java Preprocessor <inputFile>");
         }
@@ -13,23 +15,41 @@ public class Preprocessor {
         try {
             String content = new String(Files.readAllBytes(Paths.get(args[0])));
 
-            // Очистка от комментариев
-            String noBlockComments = content.replaceAll("/\\*[\\s\\S]*?\\*/", "");
-            String noComments = noBlockComments.replaceAll("//.*", "");
-
-            // Удаление лишних пробельных символов
-            String cleaned = noComments
-                    .replaceAll("[ \\t]+", " ")
-                    .replaceAll("\\r?\\n[ \\t]+", "\n")
-                    .replaceAll("[ \\t]+\\r?\\n", "\n")
-                    .replaceAll("(\\r?\\n)+", "\n");
-
-            cleaned = cleaned.trim();
+            String cleaned = cleanContent(content);
+            String message = validateAfterCleaning(cleaned);
 
             System.out.println(cleaned);
-            System.out.println("\nОшибок не выявлено");
+            System.out.println("\n" + message);
         } catch (IOException e) {
             throw new RuntimeException("Failed to open a file");
         }
+    }
+
+    private static String cleanContent(String content) {
+        // Очистка от комментариев
+        String noBlockComments = content.replaceAll("/\\*[\\s\\S]*?\\*/", "");
+        String noComments = noBlockComments.replaceAll("//.*", "");
+
+        // Удаление лишних пробельных символов
+        String cleaned = noComments
+                .replaceAll("[ \\t]+", " ")
+                .replaceAll("\\r?\\n[ \\t]+", "\n")
+                .replaceAll("[ \\t]+\\r?\\n", "\n")
+                .replaceAll("(\\r?\\n)+", "\n");
+
+        cleaned = cleaned.trim();
+
+        return cleaned;
+    }
+
+    private static String validateAfterCleaning(String cleanedContent) {
+        Pattern openCommentsPattern = Pattern.compile("/\\*|\\*/");
+        Matcher commentsMatcher = openCommentsPattern.matcher(cleanedContent);
+
+        if (commentsMatcher.find()) {
+            return "Обнаружен незакрытый многострочный комментарий!";
+        }
+
+        return "Ошибок не выявлено";
     }
 }
