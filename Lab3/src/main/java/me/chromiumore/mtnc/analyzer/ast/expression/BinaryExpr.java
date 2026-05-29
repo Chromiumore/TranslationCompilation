b80c2fef8f2a73793dc43036e72c3677215271fe
Chromiumore <parts.dima06@gmail.com>
2026-05-29 15:56:18 +0300

    binary fix

--- Lab3/src/main/java/me/chromiumore/mtnc/analyzer/ast/expression/BinaryExpr.java
+++ Lab3/src/main/java/me/chromiumore/mtnc/analyzer/ast/expression/BinaryExpr.java
@@ -2,7 +2,7 @@ package me.chromiumore.mtnc.analyzer.ast.expression;
 
 public class BinaryExpr extends Expression {
     public Expression left;
-    public String operator;  // "+", "*", "%", "..", "<", "=="
+    public String operator;  // "+", "*", "%", "<", "=="
     public Expression right;
 
     public BinaryExpr(Expression left, String op, Expression right) {
